ddb288d4693e8ac300a074e09f61f4b650202a00
Leo <16115820+Leopard20@users.noreply.github.com>
2022-07-24 19:02:29 +0430

    Added automatic update of sqfc file if it's older than the sqf one (#19)
    * Fixed build (wrong include path);
    Added automatic update of file if sqf is newer than sqfc
    
    * revert changes to json lib path; my submodules were not updated when I pulled...oof

--- src/main.cpp
+++ src/main.cpp
@@ -55,8 +55,14 @@ void processFile(ScriptCompiler& comp, std::filesystem::path path) {
 
 
         auto outputPath = outputDir / pathRelative.parent_path() / (path.stem().string() + ".sqfc");
-
-        if (std::filesystem::exists(outputPath)) return;
+        
+        //if sqfc exists, check if the sqf file has been updated (is newer). if not, skip this sqf file
+        if (std::filesystem::exists(outputPath)) { 
+            auto sqfcWriteTime = std::filesystem::last_write_time(outputPath);
+            auto sqfWriteTime = std::filesystem::last_write_time(path);
+            if (sqfWriteTime <= sqfcWriteTime) //sqf file is older than sqfc
+                return;
+        }
 
 
         std::error_code ec;
