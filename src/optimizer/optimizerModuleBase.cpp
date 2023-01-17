787394fcbfb2664af18f439358f59b527edbd189
jonpas <jonpas33@gmail.com>
2023-01-17 20:00:55 +0100

    Fix _Ptr use and non-const assignment for gcc

--- src/optimizer/optimizerModuleBase.cpp
+++ src/optimizer/optimizerModuleBase.cpp
@@ -272,10 +272,10 @@ OptimizerModuleBase::Node OptimizerModuleBase::nodeFromAST(const astnode& input)
             std::vector<std::vector<astnode>::const_iterator> lastChildren;
 
             while (!statements->children.empty()) {
-                auto& lastChild = (statements->children.end() - 1);
+                const auto& lastChild = (statements->children.end() - 1);
 
                 lastToken = lastChild->token.offset + lastChild->token.contents.size();
-                statements = lastChild._Ptr;
+                statements = &(*lastChild);
                 lastChildren.emplace_back(lastChild);
             }
 
